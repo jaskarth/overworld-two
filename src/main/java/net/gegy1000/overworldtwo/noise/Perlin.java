4639c87df1c1c779856de828f16b1c589e4718d5
SuperCoder7979 <k.pranav@gmail.com>
2020-07-28 21:35:30 -0400

    perlin gradient optimization

--- src/main/java/net/gegy1000/overworldtwo/noise/Perlin.java
+++ src/main/java/net/gegy1000/overworldtwo/noise/Perlin.java
@@ -5,6 +5,17 @@ import net.minecraft.util.math.noise.SimplexNoiseSampler;
 import java.util.Random;
 
 public final class Perlin {
+    private static final int[] GRADIENTS = new int[SimplexNoiseSampler.gradients.length * 3];
+
+    static {
+        for (int i = 0; i < SimplexNoiseSampler.gradients.length; i++) {
+            int[] grad = SimplexNoiseSampler.gradients[i];
+            GRADIENTS[(i * 3)] = grad[0];
+            GRADIENTS[(i * 3) + 1] = grad[1];
+            GRADIENTS[(i * 3) + 2] = grad[2];
+        }
+    }
+
     public static int[] initPermutationTable(Random random) {
         int[] permutations = new int[256];
         for (int i = 0; i < 256; ++i) {
@@ -22,7 +33,7 @@ public final class Perlin {
     }
 
     public static double grad(int hash, double x, double y, double z) {
-        int[] gradient = SimplexNoiseSampler.gradients[hash & 15];
-        return gradient[0] * x + gradient[1] * y + gradient[2] * z;
+        int grad = hash & 15;
+        return GRADIENTS[(grad * 3)] * x + GRADIENTS[(grad * 3) + 1] * y + GRADIENTS[(grad * 3) + 2] * z;
     }
 }
